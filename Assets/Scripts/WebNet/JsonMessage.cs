732929b962b1e31153adfef0e577a9b8dd16b311
hahayixia <1151621545@qq.com>
2021-03-19 17:13:08 +0800

    addproject

---
+++ Assets/Scripts/WebNet/JsonMessage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public abstract class JsonMessage : IMessage
+{
+    public void FromData(byte[] d)
+    {
+        string json = System.Text.Encoding.UTF8.GetString(d);
+        Newtonsoft.Json.JsonConvert.PopulateObject(json, this);
+    }
+    public void ToData(out byte[] d)
+    {
+        d = Serialize(this);
+    }
+
+    public byte[] GetBytes()
+    {
+        return Serialize(this);
+    }
+    public static byte[] Serialize<T>(T obj) where T : JsonMessage
+    {
+        string json = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        return System.Text.Encoding.UTF8.GetBytes(json);
+    }
+    public static T Deserialize<T>(byte[] data) where T : JsonMessage
+    {
+        if (data == null) return null;
+        string json = System.Text.Encoding.UTF8.GetString(data);
+
+        T t = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        return t;
+    }
+}
---
+++ Assets/Scripts/WebNet/WebChannel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class WebChannel
+{
+    public class SendStruct : IDisposable
+    {
+        public UnityWebRequest request = null;
+        public System.Action<string, byte[]> callback = null;
+
+        public void Dispose()
+        {
+            request.Dispose();
+            callback = null;
+        }
+    }
+    List<SendStruct> ssPool = null;
+
+    public WebChannel()
+    {
+        ssPool = new List<SendStruct>();
+    }
+    public void Send(string url, byte[] data, System.Action<string, byte[]> callback)
+    {
+        if (callback == null)
+        {
+            UnityWebRequest request = UnityWebRequest.Put(url, data);
+            request.method = UnityWebRequest.kHttpVerbPOST;
+            request.timeout = 30;
+            request.SendWebRequest();
+            return;
+        }
+        SendStruct ss = new SendStruct();
+        ssPool.Add(ss);
+        ss.request = UnityWebRequest.Put(url, data);
+        ss.request.method = UnityWebRequest.kHttpVerbPOST;
+        ss.request.timeout = 15;
+        ss.callback = callback;
+        ss.request.SendWebRequest();
+    }
+    public void Get(string url, System.Action<string, byte[]> callback)
+    {
+        SendStruct ss = new SendStruct();
+        ssPool.Add(ss);
+        ss.request = UnityWebRequest.Get(url);
+        ss.request.timeout = 600;
+        ss.callback = callback;
+        ss.request.SendWebRequest();
+    }
+
+    public void OnUpdate()
+    {
+        for (int i = 0; i < ssPool.Count; i++)
+        {
+            SendStruct ss = ssPool[i];
+
+            if (ss.request.isDone)
+            {
+
+                ssPool.RemoveAt(i);
+                i--;
+                if (ss.request.isHttpError || ss.request.isNetworkError)
+                {
+                    ss.callback(ss.request.error, null);
+                }
+                else
+                {
+                    ss.callback(null, ss.request.downloadHandler.data);
+                }
+                ss.Dispose();
+            }
+        }
+    }
+    public void Clear()
+    {
+        for (int i = 0; i < ssPool.Count; i++)
+        {
+            ssPool[i].Dispose();
+        }
+        ssPool.Clear();
+    }
+}
